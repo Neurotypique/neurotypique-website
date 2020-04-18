bc276d694852369fc30a7fc6fbb68b3178b4baf8
Sébastien Mercier <hello@sebastienercier.io>
2020-04-18 16:08:27 +0200

    création page work et suppression about

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/work">Work</router-link>
+      <span>
+        blabla
+      </span>
     </div>
     <router-view />
   </div>
@@ -10,14 +13,18 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+  color: #000000;
 }
 
-#nav {
+</style>
+ 
+ <style scoped>
+
+ #nav {
   padding: 30px;
 }
 
@@ -29,4 +36,9 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-</style>
+
+span {
+color:red;
+
+}
+ </style>
---
+++ src/views/Work.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+
+
+  <span>
+mes jolis projets
+  </span>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Work",
+  
+};
+</script>
+
+<style scoped>
+
+span {
+color:teal;
+
+}
+
+ </style>
